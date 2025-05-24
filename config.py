0fca0c3543a127e33f88933624321162818939af
Dimo_Edgard <20609098+DimoEdgrad@users.noreply.github.com>
2025-05-24 14:50:43 +0330

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TELEGRAM_API_TOKEN = os.environ.get("TELEGRAM_API_TOKEN")
+TELEGRAM_API_TOKEN = os.environ.get("6489239711:AAHdUIHNoBj2az2azuLXfnPM_KV3t_2MDrs")
 PAYMENT_PROVIDER_TOKEN = os.environ.get("PAYMENT_PROVIDER_TOKEN")
 OVPN_FILE_PATH = os.environ.get("OVPN_FILE_PATH")
 WG_FILE_PATH = os.environ.get("WG_FILE_PATH")
